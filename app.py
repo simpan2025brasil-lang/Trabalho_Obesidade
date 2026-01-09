0fc082e17e64af6e30d1cad60bf14400de708548
simpan2025brasil-lang <simpan2025brasil@gmail.com>
2026-01-09 12:29:54 -0300

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Teste", layout="centered")
-
-st.title("🚀 App básico funcionando")
-st.write("Se você está vendo isso, o Streamlit subiu corretamente.")
